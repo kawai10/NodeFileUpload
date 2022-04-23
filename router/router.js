5cc15d92ff5cbb2c11719d3c31e4473cb69df541
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-04-23 17:33:26 +0900

    세부 Route 경로 추가

--- router/router.js
+++ router/router.js
@@ -1,4 +1,10 @@
 "use strict";
 import express from "express";
+import { filesUploadRouter } from "../src/filesUpload/router.js";
+import { userRouter } from "../src/users/router.js";
 
 export const uploadRouter = express.Router();
+
+uploadRouter.use("/upload"), filesUploadRouter;
+
+uploadRouter.use("/users", userRouter);
